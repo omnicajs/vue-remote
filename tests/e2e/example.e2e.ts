f05f6f826a66cad48e676f597ec92cd54d44832f
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-02-21 15:32:28 +0300

    tests: Added playwright test

--- tests/e2e/example.e2e.ts
+++ tests/e2e/example.e2e.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test'
 
 test('showed hidden text after button was clicked', async ({ page }) => {
-  await page.goto('http://localhost:3000/host')
+  await page.goto('http://localhost:3000/host/1')
 
   await page.getByRole('button', { name: 'Do' }).click()
   await expect(page.getByText('hidden text')).toBeVisible()
