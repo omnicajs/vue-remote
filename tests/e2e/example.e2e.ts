e01aa6dc5bfb778ef856f4c23f1a1fc4f7cd513c
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-02-22 19:06:58 +0300

    chore: Added utils captureCoverage

--- tests/e2e/example.e2e.ts
+++ tests/e2e/example.e2e.ts
@@ -1,7 +1,10 @@
 import { test, expect } from '@playwright/test'
+import { captureCoverage } from './utils'
 import process from 'process'
 
 const SERVER = process.env.SERVER || 'localhost'
+
+captureCoverage(test)
 
 test('showed hidden text after button was clicked', async ({ page }) => {
   await page.goto(`http://${SERVER}:3000/host/1`)
