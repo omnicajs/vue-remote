d0e8dbab0e04d16472f9f23807cd390a0497bf4e
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-02-22 19:06:58 +0300

    chore: Added utils captureCoverage

--- tests/e2e/example.e2e.ts
+++ tests/e2e/example.e2e.ts
@@ -1,4 +1,7 @@
 import { test, expect } from '@playwright/test'
+import { captureCoverage } from './utils'
+
+captureCoverage(test)
 
 test('showed hidden text after button was clicked', async ({ page }) => {
   await page.goto('http://localhost:3000/host/1')
