b8151bf855c3e6e40b6a0c846b9700ea49213cde
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-03-01 16:47:00 +0300

    chore: Moved and updated fixtures

---
+++ tests/integration/fixtures/host/VButton.vue
@@ -0,0 +1,16 @@
+<template>
+    <button :disabled="disabled" @click="$emit('click')">
+        <slot />
+    </button>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+defineEmits(['click'])
+</script>
---
+++ tests/integration/fixtures/host/VInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+      type="text"
+      :value="value"
+      @input="$emit('update:value', $event.target.value)"
+  />
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  value: {
+    type: String,
+    default: '',
+  },
+})
+
+defineEmits(['update:value'])
+</script>
