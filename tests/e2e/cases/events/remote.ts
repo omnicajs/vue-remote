695598b6e4fa227a0ee475693f297fe56c2ae745
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-03-01 16:10:30 +0300

    chore: Event examples host and remote apps

---
+++ tests/e2e/cases/events/remote.ts
@@ -0,0 +1,81 @@
+import type { RemoteChannel } from '@remote-ui/core'
+import type { Component } from 'vue'
+
+import {
+  defineComponent,
+  h,
+  ref,
+} from 'vue'
+
+import {
+  createEndpoint,
+  fromInsideIframe,
+  release,
+  retain,
+} from '@remote-ui/rpc'
+
+import {
+  createRemoteRenderer,
+  createRemoteRoot,
+  defineRemoteComponent,
+} from '@/index'
+
+const createApp = async <
+  Props extends Record<string, unknown> | undefined = undefined
+>(channel: RemoteChannel, component: Component<Props>) => {
+  const remoteRoot = createRemoteRoot(channel, {
+    components: [
+      'VButton',
+      'VInput',
+    ],
+  })
+
+  await remoteRoot.mount()
+
+  const app = createRemoteRenderer(remoteRoot).createApp(component)
+
+  app.mount(remoteRoot)
+
+  return app
+}
+
+let onRelease = () => {}
+
+const endpoint = createEndpoint(fromInsideIframe())
+
+const VButton = defineRemoteComponent('VButton')
+const VInput = defineRemoteComponent('VInput', [
+  'update:value',
+] as unknown as {
+    'update:value': (value: string) => true,
+})
+
+endpoint.expose({
+  async run (channel) {
+    retain(channel)
+
+    const app = await createApp(channel, defineComponent({
+      setup () {
+        const text = ref('')
+
+        return () => [
+          h(VInput, { 'onUpdate:value': (value: string) => text.value = value, value: text.value, placeholder: 'vue-remote' }),
+          h(VButton, { onClick: () => {
+            text.value = ''
+            console.log('text.value', text.value)
+          } }, 'Clear'),
+        ]
+      },
+    }))
+
+    onRelease = () => {
+      release(channel)
+
+      app.unmount()
+    }
+  },
+
+  release () {
+    onRelease()
+  },
+})
