51d13a57b6f3436596867812ff5b7b3c5c3f8742
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-04-09 18:30:45 +0300

    chore: Added playwright, playwright.config.ts and test example
    Added packages in devDependencies:
    * @playwright/test
    * playwright

---
+++ tests/e2e/events.e2e.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test'
+import process from 'process'
+
+const SERVER = process.env.SERVER || 'localhost'
+
+test('clear button removes text from input', async ({ page }) => {
+  await page.goto(`http://${SERVER}:3000/host/events`)
+
+  await page.getByPlaceholder('vue-remote').fill('playwright@microsoft.com')
+  
+  await expect(page.getByPlaceholder('vue-remote')).toHaveValue('playwright@microsoft.com')
+
+  await page.getByRole('button', { name: 'Clear' }).click()
+
+  await expect(page.getByPlaceholder('vue-remote')).toHaveValue('')
+})
