e740849ad3844c673a1d4c405500f0031d957bd7
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2024-04-27 12:13:48 +0400

    test: Named slots

---
+++ tests/integration/fixtures/host/VCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <section
+        :id="id"
+        :aria-labelledby="'title' in $slots ? id + '-title' : undefined"
+    >
+        <div
+            v-if="'title' in $slots"
+            :id="id + '-title'"
+        >
+            <slot name="title" />
+        </div>
+
+        <slot />
+    </section>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+})
+</script>
