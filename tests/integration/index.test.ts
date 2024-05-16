2c571dfce6e0d3384c5c33f519048fe41123cc05
magomedov@retailcrm.ru <magomedov@retailcrm.ru>
2024-05-16 19:20:06 +0300

    test: Throw error doesn't support method when it's not a function

--- tests/integration/index.test.ts
+++ tests/integration/index.test.ts
@@ -280,7 +280,7 @@ describe('vue', () => {
     } as SerializedMouseEvent)
   })
 
-  test('throw error doesn\'t support method', async () => {
+  test('throw error doesn\'t support method when component doesn\'t have it', async () => {
     const receiver = createReceiver()
 
     createHostApp(receiver).mount(el as HTMLElement)
@@ -303,6 +303,31 @@ describe('vue', () => {
     }, receiver)
 
     await expect(() => vm.click()).rejects.toThrowError('Node [ID=1, KIND=component, TYPE=button] doesn\'t support method change')
+  })
+
+  test('throw error doesn\'t support method when it\'s not a function', async () => {
+    const receiver = createReceiver()
+
+    createHostApp(receiver).mount(el as HTMLElement)
+
+    const onClick = vi.fn()
+
+    const { vm } = await createRemoteApp<{ click: () => Promise<void> }>({
+      setup (_, { expose }) {
+        const button = ref<{ invoke: (method: string) => Promise<void> } | null>(null)
+
+        expose({
+          click: () => button.value?.invoke('disabled') ?? Promise.resolve(),
+        })
+
+        return () => h('button', {
+          ref: button,
+          onClick,
+        }, 'Click me')
+      },
+    }, receiver)
+
+    await expect(() => vm.click()).rejects.toThrowError('Node [ID=1, KIND=component, TYPE=button] doesn\'t support method disabled')
   })
 
   test('can reuse existing HostingTree if receiver was replaced', async () => {
