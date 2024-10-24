462bf0523b06df5853faba6ad8151e121f063d39
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2024-10-24 14:10:23 +0400

    style: Fixed code style according to eslint

--- tests/e2e/rollup.build.mjs
+++ tests/e2e/rollup.build.mjs
@@ -18,93 +18,92 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const timer = new Timer()
 
 const getDirectories = source =>
-    readdirSync(source, { withFileTypes: true })
-        .filter(dirent => dirent.isDirectory())
-        .map(dirent => dirent.name)
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
 
 const output = (dirPath, filename) => ({
-    exports: 'named',
-    format: 'esm',
-    dir: path.join(dirPath, 'dist'),
-    entryFileNames: filename + '.js',
-    sourcemap: true,
+  exports: 'named',
+  format: 'esm',
+  dir: path.join(dirPath, 'dist'),
+  entryFileNames: filename + '.js',
+  sourcemap: true,
 })
 
 const plugins = [
-    replace({
-        preventAssignment: true,
-        'process.env.NODE_ENV': JSON.stringify('production'),
-        'process.env.SERVER': process.env.SERVER || 'localhost',
-        __buildDate__: () => JSON.stringify(new Date()),
-    }),
-    alias({
-        entries: [{
-            find: '@',
-            replacement: path.resolve(__dirname, '../..', 'src'),
-        },
-        {
-            find: '~tests',
-            replacement: path.resolve(__dirname, '../..', 'tests'),
-        }],
-    }),
-    vue(),
-    resolve({
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
-    }),
-    typescript(),
+  replace({
+    preventAssignment: true,
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.env.SERVER': process.env.SERVER || 'localhost',
+    __buildDate__: () => JSON.stringify(new Date()),
+  }),
+  alias({
+    entries: [{
+      find: '@',
+      replacement: path.resolve(__dirname, '../..', 'src'),
+    },
+    {
+      find: '~tests',
+      replacement: path.resolve(__dirname, '../..', 'tests'),
+    }],
+  }),
+  vue(),
+  resolve({
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+  }),
+  typescript(),
 ]
 
 const configOptions = (dirPath, filename) => ({
-    input: path.join(dirPath, filename + '.ts'),
-    output: [output(dirPath, filename)],
-    plugins,
+  input: path.join(dirPath, filename + '.ts'),
+  output: [output(dirPath, filename)],
+  plugins,
 })
 
 async function build({ output: outputOptionsList, ...inputOptions }) {
-    let bundle
+  let bundle
 
-    try {
-        bundle = await rollup(inputOptions)
+  try {
+    bundle = await rollup(inputOptions)
 
-        await Promise.all(outputOptionsList.map(bundle.write))
-    } catch (e) {
-        console.error(e)
-    }
+    await Promise.all(outputOptionsList.map(bundle.write))
+  } catch (e) {
+    console.error(e)
+  }
 
-    if (bundle) {
-        await bundle.close()
-    }
+  if (bundle) {
+    await bundle.close()
+  }
 }
 
 const cleanDir = dirPath => rm(path.join(dirPath, 'dist'), { force: true, recursive: true })
 
 const buildCases = async () => {
-    const directories = getDirectories(path.join(__dirname, 'cases'))
+  const directories = getDirectories(path.join(__dirname, 'cases'))
 
-    for (const directory of directories) {
-        if (!directory) {
-            continue
-        }
-        
-        const dirPath = path.join(__dirname, 'cases', directory)
-
-        timer.start(directory)
-        
-        await cleanDir(dirPath)
-
-        console.info(`clean dist directory ${directory} was completed`)
-        console.info(`host.ts and remote.ts builds have been started`)
-        
-        await build(configOptions(dirPath, 'host'))
-        await build(configOptions(dirPath, 'remote'))
-        
-        console.info(`host.ts and remote.ts builds were completed in ${millisecondsToTime(timer.end(directory))}`)
+  for (const directory of directories) {
+    if (!directory) {
+      continue
     }
+        
+    const dirPath = path.join(__dirname, 'cases', directory)
+
+    timer.start(directory)
+        
+    await cleanDir(dirPath)
+
+    console.info(`clean dist directory ${directory} was completed`)
+    console.info('host.ts and remote.ts builds have been started')
+        
+    await build(configOptions(dirPath, 'host'))
+    await build(configOptions(dirPath, 'remote'))
+        
+    console.info(`host.ts and remote.ts builds were completed in ${millisecondsToTime(timer.end(directory))}`)
+  }
 }
 
-
 try {
-    await buildCases()
+  await buildCases()
 } catch (e) {
-    console.log(e)
+  console.log(e)
 }
