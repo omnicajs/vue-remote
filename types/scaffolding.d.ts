c0567c8748f59df3de0d210c27a4ce2bcb06f9b9
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2025-11-21 16:04:34 +0400

    fix: UnknownMethods signature

--- types/scaffolding.d.ts
+++ types/scaffolding.d.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type AnyFunction = (...payload: any[]) => Promise<unknown>
+
 export type None = Record<string, never>
 export type Unknown = Record<string, unknown>
 export type UnknownFunction = (...args: unknown[]) => unknown
-export type UnknownMethods = Record<string, (...payload: unknown[]) => Promise<unknown>>
+export type UnknownMethods = Record<string, AnyFunction>
 
 export type NonOptionalKeys<T> = {
   [K in keyof T]-?: undefined extends T[K] ? never : K;
