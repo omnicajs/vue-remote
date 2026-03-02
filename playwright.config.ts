96de78020f1d92ad2c99787bcc0064ffacfebc0f
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2026-03-02 13:18:39 +0400

    chore: Artifacts location

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,11 +3,12 @@ import { defineConfig, devices } from '@playwright/test'
 export default defineConfig({
   testDir: './tests/e2e',
   testMatch: '**/*.@(e2e|test).?(c|m)[jt]s?(x)',
+  outputDir: 'artifacts/playwright/results',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { outputFolder: 'artifacts/playwright/report' }]],
   use: {
     trace: 'on-first-retry',
   },
