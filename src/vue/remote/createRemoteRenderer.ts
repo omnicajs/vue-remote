064eeb5d7b4928cb0967e19b15287b894e33cfbf
Igor Kretov <knigor1337@gmail.com>
2026-03-02 10:01:42 +0300

    feat: Added static SVG vnode rendering support

--- src/vue/remote/createRemoteRenderer.ts
+++ src/vue/remote/createRemoteRenderer.ts
@@ -1,9 +1,11 @@
 import type {
   RemoteComment,
   RemoteComponent,
+  RemoteFragment,
   RemoteRoot,
   RemoteText,
 } from '@/dom/remote'
+import type { ElementNamespace } from 'vue'
 
 import { createRenderer } from 'vue'
 
@@ -17,6 +19,11 @@ type Node<Root extends RemoteRoot = RemoteRoot> =
   | Component<Root>
   | RemoteComment<Root>
   | RemoteText<Root>
+
+type Parent<Root extends RemoteRoot = RemoteRoot> = Component<Root> | Root | RemoteFragment<Root>
+
+const SVG_NAMESPACE = 'http://www.w3.org/2000/svg'
+const MATH_ML_NAMESPACE = 'http://www.w3.org/1998/Math/MathML'
 
 const nextSibling = <Root extends RemoteRoot = RemoteRoot>(node: Node<Root>) => {
   const { parent } = node
@@ -53,6 +60,80 @@ const setText = <Root extends RemoteRoot = RemoteRoot>(
   }
 }
 
+const parseStaticContent = (
+  content: string,
+  namespace: ElementNamespace
+): ChildNode[] => {
+  if (namespace === 'svg' || namespace === 'mathml') {
+    const parser = new DOMParser()
+    const namespaceUri = namespace === 'svg' ? SVG_NAMESPACE : MATH_ML_NAMESPACE
+    const wrapped = `<root xmlns="${namespaceUri}">${content}</root>`
+    const document = parser.parseFromString(wrapped, 'application/xml')
+    const root = document.documentElement
+
+    return [...root.childNodes]
+  }
+
+  const template = document.createElement('template')
+  template.innerHTML = content
+
+  return [...template.content.childNodes]
+}
+
+const createFromDOMNode = <Root extends RemoteRoot = RemoteRoot>(
+  root: Root,
+  node: ChildNode
+): Node<Root> | null => {
+  if (node.nodeType === Node.TEXT_NODE) {
+    return root.createText(node.textContent ?? '') as Node<Root>
+  }
+
+  if (node.nodeType === Node.COMMENT_NODE) {
+    return root.createComment(node.textContent ?? '') as Node<Root>
+  }
+
+  if (node.nodeType !== Node.ELEMENT_NODE) {
+    return null
+  }
+
+  const element = node as Element
+  const properties = [...element.attributes].reduce<Record<string, string>>((acc, attribute) => {
+    acc[attribute.name] = attribute.value
+    return acc
+  }, {})
+
+  const component = root.createComponent(element.localName, properties) as Component<Root>
+  const children = [...element.childNodes]
+    .map(child => createFromDOMNode(root, child))
+    .filter((child): child is Node<Root> => child != null)
+
+  children.forEach(child => component.append(child))
+
+  return component
+}
+
+const insertStaticContent = <Root extends RemoteRoot = RemoteRoot>(
+  root: Root,
+  content: string,
+  parent: Parent<Root>,
+  anchor: Node<Root> | null,
+  namespace: ElementNamespace
+): [Node<Root>, Node<Root>] => {
+  const nodes = parseStaticContent(content, namespace)
+    .map(node => createFromDOMNode(root, node))
+    .filter((node): node is Node<Root> => node != null)
+
+  if (nodes.length === 0) {
+    const placeholder = root.createText('') as Node<Root>
+    parent.insertBefore(placeholder, anchor)
+    return [placeholder, placeholder]
+  }
+
+  nodes.forEach((node) => parent.insertBefore(node, anchor))
+
+  return [nodes[0], nodes[nodes.length - 1]]
+}
+
 export default <Root extends RemoteRoot = RemoteRoot>(root: Root) => createRenderer<
   Node<Root>,
   Component<Root> | Root
@@ -74,4 +155,8 @@ export default <Root extends RemoteRoot = RemoteRoot>(root: Root) => createRende
   nextSibling,
   setText,
   setElementText,
+
+  insertStaticContent: (content, parent, anchor, namespace) => {
+    return insertStaticContent(root, content, parent as Parent<Root>, anchor as Node<Root> | null, namespace)
+  },
 })
